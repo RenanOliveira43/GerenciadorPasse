753c19c30cbc8e331dd2836ef963b8e64028ea77
Renan <renanneves430@gmail.com>
2024-12-04 22:19:20 -0300

    MainApp classe

---
+++ app/src/main/java/Gerenciadorpasse/MainApp.java
@@ -0,0 +1,35 @@
+package Gerenciadorpasse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+    protected static Stage stage;
+    //protected static Database db = new Database(true);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        setScene("/telaInicial.fxml");
+        primaryStage.show();
+    }
+
+    public static void setScene(String fxml) {
+        try {
+            Parent root = FXMLLoader.load(MainApp.class.getResource(fxml));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(MainApp.class.getResource("/style.css").toExternalForm());
+            stage.setScene(scene);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
