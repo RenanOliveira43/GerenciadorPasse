cf77cb7a291619e233aa3bb878feec2dd197213e
Renan <renanneves430@gmail.com>
2024-12-07 02:31:34 -0300

    temos uma tela nova

--- app/src/main/java/Gerenciadorpasse/TelaInicialController.java
+++ app/src/main/java/Gerenciadorpasse/TelaInicialController.java
@@ -1,5 +1,10 @@
 package gerenciadorpasse;
 
+import javafx.fxml.FXML;
+
 public class TelaInicialController {
-    
+    @FXML
+    public void carregarTelaPrincipal(){
+        MainApp.setScene("/telaPrincipal.fxml");
+    }
 }
