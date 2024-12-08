6f787c3dc76e3d892d839cbeebd04e21b8eda9bf
Renan <renanneves430@gmail.com>
2024-12-08 01:54:33 -0300

    tela de login

--- app/src/main/java/Gerenciadorpasse/User.java
+++ app/src/main/java/Gerenciadorpasse/User.java
@@ -1,7 +1,7 @@
 package gerenciadorpasse;
 
 public class User {
-    private String name;
+    private String userName;
     private boolean isFirstAccess;
     private TipoPassagem TipoPassagem;
     private double gastoMes;
