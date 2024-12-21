1ec159acaaa8abc888d30dd2143cd1f359d74f71
RenanVM <renanneves430@gmail.com>
2024-12-20 21:09:41 -0300

    sei la

---
+++ gerenciadorpasse/target/gluonfx/aarch64-android/gvm/GerenciadorPasse/com.mycompany.sample.gerenciadorpasse.mainapp_dynamic.h
@@ -0,0 +1,16 @@
+#ifndef __COM_MYCOMPANY_SAMPLE_GERENCIADORPASSE_MAINAPP_H
+#define __COM_MYCOMPANY_SAMPLE_GERENCIADORPASSE_MAINAPP_H
+
+#include <graal_isolate_dynamic.h>
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef int (*run_main_fn_t)(int argc, char** argv);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
