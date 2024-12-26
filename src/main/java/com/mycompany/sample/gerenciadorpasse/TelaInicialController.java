58b6f3f05792cbb50ed64c732e8b5ff677af1d17
RenanVM <renanneves430@gmail.com>
2024-12-26 04:36:28 -0300

    small fxml changes

--- src/main/java/com/mycompany/sample/gerenciadorpasse/TelaInicialController.java
+++ src/main/java/com/mycompany/sample/gerenciadorpasse/TelaInicialController.java
@@ -1,5 +1,6 @@
 package com.mycompany.sample.gerenciadorpasse;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import com.mycompany.sample.Utils.RandomAvatarImageGenerator;
 import javafx.fxml.FXML;
@@ -7,7 +8,16 @@ import javafx.fxml.FXML;
 public class TelaInicialController {    
     protected User user;
     @FXML private TextField nameTextField;
+    @FXML private Label welcomeLabel;
+
     
+    @FXML
+    public void initialize() {
+        if (MainApp.db.getUsers().isEmpty()) {
+            welcomeLabel.setText("Bem vindo!");
+        }
+    }
+
     @FXML
     public void carregarTelaEscolhaPassagem(){
         MainApp.setScene("/telaEscolhaPassagem.fxml");
