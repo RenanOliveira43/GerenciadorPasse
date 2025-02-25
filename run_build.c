8b70134ac00c0944716d7bf62bcfa5fe32a2ce9c
RenanVM <renanneves430@gmail.com>
2025-02-24 23:32:07 -0300

    new build script

--- run_build.c
+++ run_build.c
@@ -3,19 +3,30 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+    char* argument_list[] = {"mvn gluonfx:build gluonfx:package -e", "mvn gluonfx:install gluonfx:nativerun -e"};
+    int n = sizeof(argument_list) / sizeof(argument_list[0]);
+
     if (argc < 2) {
-        printf("Comando invalido\n");
+        fprintf(stderr, "Use: %s -b|-i|-a\n", argv[0]);
         exit(1);
     }
     else {
+        //setenv("GRAALVM_HOME", "/home/renan/Downloads/graalvm", 1);
+
         if (strcmp("-b", argv[1]) == 0){
-            system("mvn gluonfx:build gluonfx:package -e"); // build
+            system(argument_list[0]); // build
         }
-        if (strcmp("-i", argv[1]) == 0) {
-            system("mvn gluonfx:install gluonfx:nativerun -e"); // install
+        else if (strcmp("-i", argv[1]) == 0) {
+            system(argument_list[1]); // install
         }
-        if (strcmp("-a", argv[1]) == 0) {
-            system("mvn gluonfx:build gluonfx:package gluonfx:install gluonfx:nativerun -e"); // build and install
+        else if (strcmp("-a", argv[1]) == 0) {
+            for (int i = 0; i < n; i++) {
+                system(argument_list[i]); // build and install
+            }
+        }
+        else {
+            fprintf(stderr, "Comando invalido: %s\nOpções validas: -b (build), -i (install), -a (all)\n", argv[1]);
+            exit(1);
         }
     }
 }
