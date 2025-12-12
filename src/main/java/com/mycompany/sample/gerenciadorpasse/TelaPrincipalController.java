875d040a3d02ae3bf9f4929121db2e95539e4dfe
RenanUbuntu <renanneves430@gmail.com>
2025-12-11 22:10:49 -0300

    update

--- src/main/java/com/mycompany/sample/gerenciadorpasse/TelaPrincipalController.java
+++ src/main/java/com/mycompany/sample/gerenciadorpasse/TelaPrincipalController.java
@@ -128,10 +128,13 @@ public class TelaPrincipalController {
     private void subtrairValorPassagem() {
         MainApp.db.getUsers().get(indexUsuarioAtual).passagem.subtrairValorPassagem();
         MainApp.db.getUsers().get(indexUsuarioAtual).passagem.somarGastoMes(MainApp.db.getUsers().get(indexUsuarioAtual).passagem.getTipoPassagem().getValor());
-        MainApp.db.getUsers().get(indexUsuarioAtual).usosPassagem.add(LocalDateTime.now().toString());
-
-        MainApp.db.update(MainApp.db.getUsers().get(0));
         
+        if (MainApp.db.getUsers().get(indexUsuarioAtual).getPassagem().getSaldo() > 0) {
+            MainApp.db.getUsers().get(indexUsuarioAtual).usosPassagem.add(LocalDateTime.now().toString());
+        }
+
+        MainApp.db.update(MainApp.db.getUsers().get(indexUsuarioAtual));
+
         saldoLabel.setText(String.format("R$ %.2f", MainApp.db.getUsers().get(indexUsuarioAtual).passagem.getSaldo()));
     }
 
